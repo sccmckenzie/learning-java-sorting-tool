155d2fa9c4d054b94cf60dddb8e0b5da487592b9
scottmckenzie <sccmckenzie@gmail.com>
2023-04-22 21:42:57 -0500

    add invalid arg handling

--- Sorting Tool (Java)/task/src/sorting/Main.java
+++ Sorting Tool (Java)/task/src/sorting/Main.java
@@ -2,6 +2,7 @@ package sorting;
 
 import java.util.*;
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
 
 
 public class Main {
@@ -12,9 +13,22 @@ public class Main {
         Context context = new Context();
 
 
+        for (String arg : args) {
+            if (arg.contains("-") && !(arg.contains("dataType") || arg.contains("sortingType"))) {
+                System.out.println("\"" + arg + "\" is not a valid parameter. It will be skipped.");
+            }
+        }
+
         AppConfig config = new AppConfig();
         JCommander jcmd = JCommander.newBuilder().addObject(config).build();
-        jcmd.parse(args);
+
+        try {
+            jcmd.parse(args);
+        } catch (ParameterException e) {
+            if (e.getMessage().contains("dataType") || e.getMessage().contains("sortingType")) {
+                return;
+            }
+        }
 
         switch(config.dataType) {
             case "long":
